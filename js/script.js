aa55ae2de4ebe887e27269fdf2a92b9b294201d6
Manuel Meiriño <mmeirino@udesa.edu.ar>
2024-08-07 16:59:01 -0300

    case insensitive + minor index adjustments

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Define the passwords for each panel
     const passwords = [
-        'juanchi', 
-        'topa', 
+        'patito', 
+        'Zt6Xw8Gf', 
         'sV2B4LzY', 
         'N9cQ1dR5', 
         'mH7Tp6Wx',
@@ -15,40 +14,29 @@ document.addEventListener('DOMContentLoaded', () => {
         'E1cQ8Zr3'
     ];
 
-    // Function to check the password
     function checkPassword(panelNumber) {
-        // Get the entered password and the correct password
-        const enteredPassword = document.getElementById(`password${panelNumber}`).value;
-        const correctPassword = passwords[panelNumber - 1]; // panelNumber is 1-based
+        const enteredPassword = document.getElementById(`password${panelNumber}`).value.toLowerCase();
+        const correctPassword = passwords[panelNumber - 1].toLowerCase();
+        const panel = document.getElementById(`panel${panelNumber}`);
 
         if (enteredPassword === correctPassword) {
-            // Unlock the panel
-            const panel = document.getElementById(`panel${panelNumber}`);
-            panel.classList.remove('expecting', 'shake', 'wrong');
+            panel.classList.remove('expecting', 'error');
             panel.classList.add('unlocked');
             document.getElementById(`clue${panelNumber}`).style.display = 'block';
 
-            // Disable the current panel's input and enable the next panel's input
             if (panelNumber < passwords.length) {
                 document.getElementById(`password${panelNumber + 1}`).removeAttribute('disabled');
-                const nextPanel = document.getElementById(`panel${panelNumber + 1}`);
-                nextPanel.classList.add('expecting');
+                document.getElementById(`panel${panelNumber + 1}`).classList.add('expecting');
             }
         } else {
-            // Show the wrong password animation
-            const panel = document.getElementById(`panel${panelNumber}`);
-            panel.classList.add('shake', 'wrong');
-            setTimeout(() => {
-                panel.classList.remove('shake', 'wrong');
-            }, 1000);
+            panel.classList.add('error');
+            setTimeout(() => panel.classList.remove('error'), 1000); // Remove error class after 1 second
         }
     }
 
-    // Initially disable all inputs except for the first one
     for (let i = 2; i <= passwords.length; i++) {
         document.getElementById(`password${i}`).setAttribute('disabled', 'true');
     }
 
-    // Make checkPassword function available globally
     window.checkPassword = checkPassword;
 });
